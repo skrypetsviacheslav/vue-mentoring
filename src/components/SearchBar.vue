9911ab6fd32aa22d5ddcbdc0993a65a3f2591b0a
Slava <driew123@gmail.com>
2021-03-19 21:30:11 +0200

    add SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="search-bar">
+    <div class="form-group form-inline">
+      <input
+        type="text"
+        v-model="searchText"
+        class="form-control text-white bg-dark border-dark"
+        placeholder="Search"
+      />
+      <button
+        type="button"
+        class="m-3 btn"
+        @click="$emit('button-clicked', searchText, selectedOption)"
+      >
+        Search
+      </button>
+    </div>
+    <Toogle
+      label="SEARCH BY"
+      :firstOption="firstOption"
+      :secondOption="secondOption"
+      @value-switched="seachByOptionChanged"
+    />
+  </div>
+</template>
+
+<script>
+import Toogle from "./Toogle.vue";
+
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      selectedOption: this.firstOption,
+      searchText: ""
+    };
+  },
+  components: { Toogle },
+  props: {
+    firstOption: {
+      type: String,
+      require: true
+    },
+    secondOption: {
+      type: String,
+      require: true
+    }
+  },
+  methods: {
+    seachByOptionChanged(newValue) {
+      this.selectedOption = newValue;
+    }
+  }
+};
+</script>
+<style scoped>
+.btn {
+  color: white;
+  background-color: #f65261;
+}
+</style>
---
+++ tests/unit/SearchBar.spec.js
@@ -0,0 +1,81 @@
+import { mount } from "@vue/test-utils";
+import SearchBar from "../../src/components/SearchBar.vue";
+import Toogle from "../../src/components/Toogle.vue";
+
+const firstOption = "Test first option";
+const secondOption = "Test second option";
+const factory = propsData => {
+  return mount(SearchBar, {
+    propsData: {
+      firstOption: firstOption,
+      secondOption: secondOption,
+      ...propsData
+    }
+  });
+};
+
+describe("SearchBar.vue", () => {
+  it("renders correctly", () => {
+    const wrapper = mount(SearchBar);
+
+    expect(wrapper.vm.$el).toMatchSnapshot();
+  });
+
+  it("displays a passed properties", () => {
+    const wrapper = factory();
+
+    const SearchBar = wrapper.find(".search-bar");
+    const Toogle = SearchBar.find(".toogle");
+    expect(Toogle.text()).toContain(firstOption);
+    expect(Toogle.text()).toContain(secondOption);
+  });
+
+  it("reflect to user input", async () => {
+    const testInput = "some value";
+    const wrapper = factory();
+    const SearchBar = wrapper.find(".search-bar");
+    const SearchInput = SearchBar.find(".form-group input[type='text']");
+
+    await SearchInput.setValue(testInput);
+
+    expect(wrapper.vm.searchText).toEqual(testInput);
+  });
+
+  it("reflect to user input", async () => {
+    const testInput = "some value";
+    const wrapper = factory();
+    const SearchBar = wrapper.find(".search-bar");
+    const SearchInput = SearchBar.find(".form-group input[type='text']");
+
+    await SearchInput.setValue(testInput);
+
+    expect(wrapper.vm.searchText).toEqual(testInput);
+  });
+
+  it("reflect to toogle change", () => {
+    const wrapper = factory();
+    const ToogleWrapper = wrapper.findComponent(Toogle);
+
+    ToogleWrapper.vm.$emit("value-switched", secondOption);
+
+    expect(wrapper.vm.selectedOption).toEqual(secondOption);
+  });
+
+  it("emit button-clicked event with user input and selected option when button cicked", async () => {
+    const testInput = "some value";
+    const wrapper = factory();
+    const SearchBar = wrapper.find(".search-bar");
+    const SearchInput = SearchBar.find(".form-group input[type='text']");
+    const SearchButton = SearchBar.find(".form-group button");
+
+    await SearchInput.setValue(testInput);
+    await SearchButton.trigger("click");
+
+    expect(wrapper.emitted("button-clicked")).toBeTruthy();
+    expect(wrapper.emitted("button-clicked").length).toBe(1);
+    expect(wrapper.emitted("button-clicked")[0]).toEqual([
+      testInput,
+      firstOption
+    ]);
+  });
+});
